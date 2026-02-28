075ece7159664be55d0a1eccce32eb86f3991847
mohamed_hussein <mohamedhusseinmans@gmail.com>
2026-02-28 10:40:06 +0200

    separeta validation from repo impl

--- lib/core/validation/chat_repo_validation.dart
+++ lib/core/validation/chat_repo_validation.dart
@@ -1,12 +1,32 @@
 import 'package:chat_bot/feature/chat/data/model/chat_model/gemini_chat_response.dart';
 
-extension ChatRepoValidation on ChatMessageModel {
-  bool isValid() {
-    return role != null &&
-        role!.isNotEmpty &&
-        parts != null &&
-        parts!.isNotEmpty &&
-        parts!.first.text != null &&
-        parts!.first.text!.isNotEmpty;
+mixin ChatRepoValidation {
+  outputValidation(ChatMessageModel result) {
+    if (result.role == null || result.role!.trim().isEmpty) {
+      throw StateError('missing role in response');
+    }
+    if (result.parts == null || result.parts!.isEmpty) {
+      throw StateError('missing parts in response');
+    }
+    if (result.parts!.first.text == null || result.parts!.first.text!.isEmpty) {
+      throw StateError('parts cannot be empty');
+    }
+    if (result.role != 'model') {
+      throw StateError('role must be model');
+    }
+  }
+
+  void input_validation(List<ChatMessageModel> messages) {
+    if (messages.isEmpty) {
+      throw ArgumentError('No messages provided');
+    } else if (messages.length < 1) {
+      throw ArgumentError('At least one message is required');
+    } else if (messages.last.role != 'user') {
+      throw ArgumentError('Last message must be user');
+    } else if (messages.last.text.isEmpty) {
+      throw ArgumentError('Last message content must not be empty');
+    } else if (messages.last.text.trim().isEmpty) {
+      throw ArgumentError('Last message content must not be empty');
+    }
   }
 }
--- lib/feature/chat/data/gemenai_chat_repo/gemenai_chat repo_impl.dart
+++ lib/feature/chat/data/gemenai_chat_repo/gemenai_chat repo_impl.dart
@@ -1,9 +1,10 @@
+import 'package:chat_bot/core/validation/chat_repo_validation.dart';
 import 'package:chat_bot/feature/chat/data/gemini_chat_service/gemini_chat_service.dart';
 import 'package:chat_bot/feature/chat/domain/chat_repo/chat_repo.dart';
 
 import '../model/chat_model/gemini_chat_response.dart';
 
-class GemenaiChatRepoImpl implements ChatRepo {
+class GemenaiChatRepoImpl with ChatRepoValidation implements ChatRepo  {
   final ChatApiService _chatApiService;
 
   GemenaiChatRepoImpl({required ChatApiService chatApiService})
@@ -13,33 +14,14 @@ class GemenaiChatRepoImpl implements ChatRepo {
   Future<ChatMessageModel> sendMessages({
     required List<ChatMessageModel> messages,
   }) async {
-    if (messages.isEmpty) {
-      throw ArgumentError('No messages provided');
-    } else if (messages.length < 1) {
-      throw ArgumentError('At least one message is required');
-    } else if (messages.last.role != 'user') {
-      throw ArgumentError('Last message must be user');
-    } else if (messages.last.text.isEmpty) {
-      throw ArgumentError('Last message content must not be empty');
-    } else if (messages.last.text.trim().isEmpty) {
-      throw ArgumentError('Last message content must not be empty');
-    }
+    input_validation(messages);
 
     final result = await _chatApiService.sendMessages(messages: messages);
 
-    if (result.role == null || result.role!.trim().isEmpty) {
-      throw StateError('missing role in response');
-    }
-    if (result.parts == null || result.parts!.isEmpty) {
-      throw StateError('missing parts in response');
-    }
-    if (result.parts!.first.text == null || result.parts!.first.text!.isEmpty) {
-      throw StateError('parts cannot be empty');
-    }
-    if (result.role != 'model') {
-      throw StateError('role must be model');
-    }
+  outputValidation(result);
 
     return result;
   }
+
+  
 }
